2e9df552bae8b71fd5e7f98447231b520f309c6e
BlowSea(soft) <v2884539@gmail.com>
2026-05-01 00:02:56 +0500

    main

--- app.py
+++ app.py
@@ -8,13 +8,10 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     db.init_app(app)
-    
     login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = 'main.login' 
     login_manager.login_message_category = 'info'
-
-    # 2. Создание папки для загрузок, если её нет
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
     @login_manager.user_loader
@@ -22,7 +19,6 @@ def create_app(config_class=Config):
         return User.query.get(int(user_id))
     from routes import main_bp
     app.register_blueprint(main_bp)
-
     from api import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
     with app.app_context():
